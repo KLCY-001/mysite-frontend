51b383ad2adaabb294135e1fc582cfb391d86a61
LYJ <343577362@qq.com>
2025-06-25 18:55:38 +0800

    fix: 调整 axios baseURL 配置，使用 Render 公网地址

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,9 @@
 // src/api/axios.ts
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://kclyworkspace.onrender.com'  // 改为你的后端 Render 地址
-axios.defaults.timeout = 10000  // 可选，设置超时时间
-axios.defaults.headers.post['Content-Type'] = 'application/json'
+const instance = axios.create({
+  baseURL: 'https://kclyworkspace.onrender.com', // ← 你的后端 Render 地址
+})
 
-export default axios
+export default instance
+
